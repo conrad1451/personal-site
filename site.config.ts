ba598f25ea8aa5cc8609c8de829674caf0f0cfc8
Conrad Hansen-Quartey <Chansenq@gmail.com>
2023-08-31 07:42:51 -0400

    Update Notion Page source

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '6d95a0fa0bb34e25a5df4e5f740d7701',
+  rootNotionPageId: '4f52a54ded6f4ef4b830123b7ff3f618',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
