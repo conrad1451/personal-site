d1d1b489a19de15c14e0cde3028f7f613e81e61c
Conrad Hansen-Quartey <Chansenq@gmail.com>
2023-09-20 20:07:15 -0400

    Remove Resume Link From footer
    Not necessary

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -109,10 +109,10 @@ export const FooterImpl: React.FC = () => {
             <FaGithub />
           </a>
         )}
-        <a href="https://drive.google.com/file/d/1966dZX4w8KF84IpvKdBds5ILVYiu3N-k/view?usp=sharing" 
+{/*         <a href="https://drive.google.com/file/d/1966dZX4w8KF84IpvKdBds5ILVYiu3N-k/view?usp=sharing" 
           className={styles.readme}>
           <FaReadme/>
-        </a>
+        </a> */}
         {config.linkedin && (
           <a
             className={styles.linkedin}
